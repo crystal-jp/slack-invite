671d044288390fca0a41be3255eb6bd40de34777
Pine Mizune <pinemz@gmail.com>
2015-12-31 18:45:51 +0900

    refactor(app): :hocho:

--- src/app.cr
+++ src/app.cr
@@ -3,20 +3,6 @@ require "kemal"
 require "./services/*"
 require "./controllers/*"
 
-# class App < Base::App
-#   routes.draw do
-#     all "/", "slack_invite#index"
-#     all "/en/", "slack_invite#index"
-#     post "/invite", "slack_invite#invite"
-#     register SlackInviteController
-#   end
-#
-#   settings.configure do |conf|
-#     #conf.environment = "development"
-#     conf.static_dirs = ["/assets"]
-#   end
-# end
-
 slack_invite = SlackInviteController.new
 
 get "/" { |env| slack_invite.index(env) }
